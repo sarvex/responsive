a0ff4f595fb735e7651e94e04d294c3d076034b5
Sarvex <sarvex@jatasra.com>
2020-04-04 20:34:56 +0530

    Updated Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:responsive/pages/home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './pages/home.dart';
 
-void main() => runApp(DevicePreview(builder: (context) => MyApp()));
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => EasyLocalization(
+          child: MyApp(),
+          supportedLocales: [Locale('en', 'US'), Locale('hi', 'IN')],
+          path: 'assets/langs',
+        ),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      locale: DevicePreview.of(context).locale,
-      builder: DevicePreview.appBuilder,
-      title: 'Flutter Demo',
+      title: 'Toffee',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        EasyLocalization.of(context).delegate,
+      ],
+      supportedLocales: EasyLocalization.of(context).supportedLocales,
+      locale: EasyLocalization.of(context).locale,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
+      builder: DevicePreview.appBuilder,
       home: HomePage(),
     );
   }
