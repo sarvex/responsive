329cde00a8a2ff4c066dce2a912c41dd1259fd1a
Rudresh <rudresh.narwal@housr.in>
2020-04-05 08:40:57 +0530

    updated responsive widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:toffee/pages/landing.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -11,103 +12,104 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
-  Choice _selectedChoice = choices[0];
-  PageController _pageController;
+  // int _currentIndex = 0;
+  // Choice _selectedChoice = choices[0];
+  // PageController _pageController;
 
-  void _select(Choice choice) {
-    setState(() {
-      _selectedChoice = choice;
-    });
-  }
+  // void _select(Choice choice) {
+  //   setState(() {
+  //     _selectedChoice = choice;
+  //   });
+  // }
 
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _pageController = PageController();
+  // }
 
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   _pageController.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Toffee'),
-        actions: <Widget>[
-          // action button
-          IconButton(
-            icon: Icon(choices[0].icon),
-            onPressed: () {
-              _select(choices[0]);
-            },
-          ),
-          IconButton(
-            icon: Icon(choices[1].icon),
-            onPressed: () {
-              _select(choices[1]);
-            },
-          ),
-          PopupMenuButton<Choice>(
-            onSelected: _select,
-            itemBuilder: (BuildContext context) {
-              return choices.skip(2).map((Choice choice) {
-                return PopupMenuItem<Choice>(
-                  value: choice,
-                  child: Text(choice.title),
-                );
-              }).toList();
-            },
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ChoiceCard(choice: _selectedChoice),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(FontAwesomeIcons.shoppingBasket),
-        backgroundColor: Theme.of(context).accentColor,
-        onPressed: () {},
-      ),
-      bottomNavigationBar: BottomNavyBar(
-        selectedIndex: _currentIndex,
-        onItemSelected: (index) {
-          setState(() => _currentIndex = index);
-          _pageController.jumpToPage(index);
-        },
-        items: <BottomNavyBarItem>[
-          BottomNavyBarItem(
-            title: Text('HOME'),
-            icon: Icon(FontAwesomeIcons.heartbeat),
-            activeColor: Theme.of(context).accentColor,
-            textAlign: TextAlign.right,
-          ),
-          BottomNavyBarItem(
-            title: Text('DETAILS'),
-            icon: Icon(FontAwesomeIcons.bullhorn),
-            activeColor: Theme.of(context).accentColor,
-            textAlign: TextAlign.right,
-          ),
-          BottomNavyBarItem(
-            title: Text('FAQs'),
-            icon: Icon(FontAwesomeIcons.question),
-            activeColor: Theme.of(context).accentColor,
-            textAlign: TextAlign.right,
-          ),
-          BottomNavyBarItem(
-            title: Text('CALL'),
-            icon: Icon(FontAwesomeIcons.phone),
-            activeColor: Theme.of(context).accentColor,
-            textAlign: TextAlign.right,
-          ),
-        ],
-      ),
-    );
+    return LandingPage();
+    // Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Toffee'),
+    //     actions: <Widget>[
+    //       // action button
+    //       IconButton(
+    //         icon: Icon(choices[0].icon),
+    //         onPressed: () {
+    //           _select(choices[0]);
+    //         },
+    //       ),
+    //       IconButton(
+    //         icon: Icon(choices[1].icon),
+    //         onPressed: () {
+    //           _select(choices[1]);
+    //         },
+    //       ),
+    //       PopupMenuButton<Choice>(
+    //         onSelected: _select,
+    //         itemBuilder: (BuildContext context) {
+    //           return choices.skip(2).map((Choice choice) {
+    //             return PopupMenuItem<Choice>(
+    //               value: choice,
+    //               child: Text(choice.title),
+    //             );
+    //           }).toList();
+    //         },
+    //       ),
+    //     ],
+    //   ),
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(16.0),
+    //     child: ChoiceCard(choice: _selectedChoice),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     child: Icon(FontAwesomeIcons.shoppingBasket),
+    //     backgroundColor: Theme.of(context).accentColor,
+    //     onPressed: () {},
+    //   ),
+    //   bottomNavigationBar: BottomNavyBar(
+    //     selectedIndex: _currentIndex,
+    //     onItemSelected: (index) {
+    //       setState(() => _currentIndex = index);
+    //       _pageController.jumpToPage(index);
+    //     },
+    //     items: <BottomNavyBarItem>[
+    //       BottomNavyBarItem(
+    //         title: Text('HOME'),
+    //         icon: Icon(FontAwesomeIcons.heartbeat),
+    //         activeColor: Theme.of(context).accentColor,
+    //         textAlign: TextAlign.right,
+    //       ),
+    //       BottomNavyBarItem(
+    //         title: Text('DETAILS'),
+    //         icon: Icon(FontAwesomeIcons.bullhorn),
+    //         activeColor: Theme.of(context).accentColor,
+    //         textAlign: TextAlign.right,
+    //       ),
+    //       BottomNavyBarItem(
+    //         title: Text('FAQs'),
+    //         icon: Icon(FontAwesomeIcons.question),
+    //         activeColor: Theme.of(context).accentColor,
+    //         textAlign: TextAlign.right,
+    //       ),
+    //       BottomNavyBarItem(
+    //         title: Text('CALL'),
+    //         icon: Icon(FontAwesomeIcons.phone),
+    //         activeColor: Theme.of(context).accentColor,
+    //         textAlign: TextAlign.right,
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
 
---
+++ lib/utility/assets.dart
@@ -0,0 +1,5 @@
+class Assets{
+  static const welcome = 'assets/images/welcome.svg';
+  static const logo = 'assets/images/logo.png';
+  static const family_group = 'assets/images/family-group.png';
+}
