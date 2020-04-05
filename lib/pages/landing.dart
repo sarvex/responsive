6d1c7d03bb45b1416bb0d4f9904560e76da02540
Sarvex <sarvex@jatasra.com>
2020-04-05 17:33:01 +0530

    Merged Logo and Hero

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // import 'package:responsive_widgets/responsive_widgets.dart';
-import 'sections/logo.dart';
 import 'sections/welcome.dart';
 import 'sections/hero.dart';
 
@@ -15,7 +14,7 @@ class LandingPage extends StatelessWidget {
         child: Container(
           constraints: BoxConstraints(maxWidth: 1440),
           child: ListView(
-            children: <Widget>[LogoSection(), HeroSection(), WelcomeContainer(), ],
+            children: <Widget>[HeroSection(), WelcomeContainer(), ],
           ),
         ),
       ),
--- lib/pages/sections/hero.dart
+++ lib/pages/sections/hero.dart
@@ -10,20 +10,33 @@ class HeroSection extends StatelessWidget {
       child: ClipPath(
         child: Card(
           color: Color(0xffd9fffe),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Column(
             children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Image.asset(
-                  Images.family,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset(
+                    Images.logo,
+                    height: 96.0,
+                  ),
+                ],
               ),
-              Expanded(
-                flex: 2,
-                child: Image.asset(
-                  Images.family,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Image.asset(
+                      Images.family,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Image.asset(
+                      Images.family,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -35,18 +48,21 @@ class HeroSection extends StatelessWidget {
 }
 
 class CustomClipPath extends CustomClipper<Path> {
-  var radius=48.0;
+  var radius = 48.0;
 
   @override
   Path getClip(Size size) {
     Path path = Path();
     path.lineTo(0, size.height);
-    path.arcToPoint(Offset(radius, size.height - radius), radius: Radius.circular(radius));
+    path.arcToPoint(Offset(radius, size.height - radius),
+        radius: Radius.circular(radius));
     path.lineTo(size.width - radius, size.height - radius);
-    path.arcToPoint(Offset(size.width, size.height - (2*radius)), radius: Radius.circular(radius), clockwise: false);
+    path.arcToPoint(Offset(size.width, size.height - (2 * radius)),
+        radius: Radius.circular(radius), clockwise: false);
     path.lineTo(size.width, 0);
     return path;
   }
+
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
