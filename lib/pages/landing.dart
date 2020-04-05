4b44413209beeb08a4cfc877e61e9152a81e11e3
Sarvex <sarvex@jatasra.com>
2020-04-05 17:02:37 +0530

    Added Logo & Hero

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // import 'package:responsive_widgets/responsive_widgets.dart';
+import 'sections/logo.dart';
 import 'sections/welcome.dart';
 import 'sections/hero.dart';
 
@@ -14,7 +15,7 @@ class LandingPage extends StatelessWidget {
         child: Container(
           constraints: BoxConstraints(maxWidth: 1440),
           child: ListView(
-            children: <Widget>[HeroSection(), WelcomeContainer(), ],
+            children: <Widget>[LogoSection(), HeroSection(), WelcomeContainer(), ],
           ),
         ),
       ),
--- lib/pages/sections/hero.dart
+++ lib/pages/sections/hero.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toffee/utility/assets.dart';
 
 class HeroSection extends StatelessWidget {
   const HeroSection({Key key}) : super(key: key);
@@ -6,15 +7,46 @@ class HeroSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        color: Color(0xd9fffe),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(10),
-                topRight: Radius.circular(10)),
-            side: BorderSide(width: 5, color: Colors.green)),
-        child: ListTile(),
+      child: ClipPath(
+        child: Card(
+          color: Color(0xffd9fffe),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Image.asset(
+                  Images.family,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Image.asset(
+                  Images.family,
+                ),
+              ),
+            ],
+          ),
+        ),
+        clipper: CustomClipPath(),
       ),
     );
   }
+}
+
+class CustomClipPath extends CustomClipper<Path> {
+  var radius=48.0;
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.arcToPoint(Offset(radius, size.height - radius), radius: Radius.circular(radius));
+    path.lineTo(size.width - radius, size.height - radius);
+    path.arcToPoint(Offset(size.width, size.height - (2*radius)), radius: Radius.circular(radius), clockwise: false);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
