b563ea4c112d1de5122fccfea261bc92a8a17494
Rudresh <rudresh.narwal@housr.in>
2020-04-05 11:53:22 +0530

    Added section for mobile

--- lib/utility/assets.dart
+++ lib/utility/assets.dart
@@ -1,5 +1,6 @@
 class Images {
-  static const welcome = 'images/welcome.svg';
+  static const welcome = 'images/welcome.png';
+  static const welcomeSvg = 'images/welcomeSvg.svg';
   static const logo = 'images/logo.png';
   static const family_group = 'images/family-group.png';
 }
