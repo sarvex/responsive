1a7402704beedda426c6877db12feba8b7f1d465
Sarvex <sarvex@jatasra.com>
2020-04-05 11:53:07 +0530

    Home Screen revert

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,7 @@
-// import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:toffee/pages/landing.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,85 +12,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // int _currentIndex = 0;
-  // PageController _pageController;
+  int _currentIndex = 0;
+  PageController _pageController;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _pageController = PageController();
-  // }
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
 
-  // @override
-  // void dispose() {
-  //   _pageController.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return LandingPage();
-    // Scaffold(
-    //   appBar: AppBar(
-    //     title: const Text('Toffee'),
-    //     leading: IconButton(
-    //       icon: Icon(Icons.apps),
-    //       onPressed: () {},
-    //     ),
-    //     actions: <Widget>[
-    //       // action button
-    //       IconButton(
-    //         icon: Icon(Icons.call),
-    //         onPressed: () {},
-    //       ),
-    //       IconButton(
-    //         icon: Icon(Icons.notification_important),
-    //         onPressed: () {},
-    //       ),
-    //     ],
-    //   ),
-    //   body: Padding(
-    //     padding: const EdgeInsets.all(16.0),
-    //     child: LandingPage(),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     child: Icon(FontAwesomeIcons.shoppingBasket),
-    //     backgroundColor: Theme.of(context).accentColor,
-    //     onPressed: () {},
-    //   ),
-    //   bottomNavigationBar: BottomNavyBar(
-    //     selectedIndex: _currentIndex,
-    //     onItemSelected: (index) {
-    //       setState(() => _currentIndex = index);
-    //       _pageController.jumpToPage(index);
-    //     },
-    //     items: <BottomNavyBarItem>[
-    //       BottomNavyBarItem(
-    //         title: Text('LIFE'),
-    //         icon: Icon(FontAwesomeIcons.heartbeat),
-    //         activeColor: Theme.of(context).accentColor,
-    //         textAlign: TextAlign.right,
-    //       ),
-    //       BottomNavyBarItem(
-    //         title: Text('DETAILS'),
-    //         icon: Icon(FontAwesomeIcons.bullhorn),
-    //         activeColor: Theme.of(context).accentColor,
-    //         textAlign: TextAlign.right,
-    //       ),
-    //       BottomNavyBarItem(
-    //         title: Text('FAQs'),
-    //         icon: Icon(FontAwesomeIcons.question),
-    //         activeColor: Theme.of(context).accentColor,
-    //         textAlign: TextAlign.right,
-    //       ),
-    //       BottomNavyBarItem(
-    //         title: Text('CALL'),
-    //         icon: Icon(FontAwesomeIcons.phone),
-    //         activeColor: Theme.of(context).accentColor,
-    //         textAlign: TextAlign.right,
-    //       ),
-    //     ],
-    //   ),
-    // );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Toffee'),
+        leading: IconButton(
+          icon: Icon(Icons.apps),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          // action button
+          IconButton(
+            icon: Icon(Icons.call),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.notification_important),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: LandingPage(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(FontAwesomeIcons.shoppingBasket),
+        backgroundColor: Theme.of(context).accentColor,
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        itemCornerRadius: 8,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            title: Text('LIFE'),
+            icon: Icon(FontAwesomeIcons.heartbeat),
+            activeColor: Theme.of(context).accentColor,
+            textAlign: TextAlign.right,
+          ),
+          BottomNavyBarItem(
+            title: Text('DETAILS'),
+            icon: Icon(FontAwesomeIcons.bullhorn),
+            activeColor: Theme.of(context).accentColor,
+            textAlign: TextAlign.right,
+          ),
+          BottomNavyBarItem(
+            title: Text('FAQs'),
+            icon: Icon(FontAwesomeIcons.question),
+            activeColor: Theme.of(context).accentColor,
+            textAlign: TextAlign.right,
+          ),
+          BottomNavyBarItem(
+            title: Text('CALL'),
+            icon: Icon(FontAwesomeIcons.phone),
+            activeColor: Theme.of(context).accentColor,
+            textAlign: TextAlign.right,
+          ),
+        ],
+      ),
+    );
   }
 }
